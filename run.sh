618d42ef77985ebe6ae8823d54309cd5dad15071
Ivan Remen <bhyk@yandex.ru>
2026-02-24 10:09:24 +0300

    Change workdir

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --gpus all -p 8888:8888 --shm-size=2g -v "$PWD":/workspace -v "$HOME"/otus/dz7:/notebooks llm-notebook
+docker run --rm -it --gpus all -p 8888:8888 --shm-size=2g -v "$PWD":/workspace -v "$HOME"/otus/dz6:/notebooks llm-notebook
